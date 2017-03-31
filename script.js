d7c9faa16b9861fa99073693036b2c260c8e4109
Elizabeth <elizabeth.cooper21@gmail.com>
2017-03-30 20:09:57 -0400

    added css to login screen

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
 $(document).ready(function() {
-    $('div').mouseenter(function() {
-        $('div').fadeTo('fast', 1);
-    });
-    $('div').mouseleave(function() {
-        $('div').fadeTo('fast', 0.5);
-    });
+
 });
